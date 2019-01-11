57297030badf789cb81e0f0edbf9faabd3885346
Joe Aree <jirat.onaree@gmail.com>
2019-01-11 17:58:46 +0700

    Better ticker styling

--- src/containers/Ticker.js
+++ src/containers/Ticker.js
@@ -4,19 +4,35 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 import { actionCreators } from '../reducers/ticker';
+import BTC from '../images/BTC.png';
+
+const Logo = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-top: 15px;
+  margin-left: 20px;
+`;
 
 const Pair = styled.h1`
   color: #fff;
-  margin-left: 5px;
-  margin-right: 100px;
+  margin-left: 15px;
+  background: url()
 `;
 
 const Label = styled.p`
   color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+  margin-bottom: 10px;
 `;
 
 const Value = styled.p`
-  color: #fff;
+  color: ${props => props.color || '#fff'};
+  display: inline;
+  margin-top: 0;
+  font-size: 20px;
+  font-weight: bold;
+  white-space: nowrap;
 `;
 
 class Ticker extends React.Component {
@@ -48,32 +64,50 @@ class Ticker extends React.Component {
   onError = err => console.log(err);
 
   render() {
-    const { ticker } = this.props;
+    const {
+      ticker: {
+        dailyChange,
+        dailyChangePerc,
+        lastPrice,
+        volume,
+        high,
+        low,
+      },
+    } = this.props;
 
-    if (!ticker.lastPrice) return 'Fetching..';
+    if (!lastPrice) return 'Fetching..';
 
     return (
-      <div className="row">
-        <Pair>BTC/USD</Pair>
-        <div className="col-1">
-          <Label>Last Price</Label>
-          <Value>{ticker.lastPrice}</Value>
+      <div className="row justify-between">
+        <div className="row">
+          <Logo src={BTC} alt="BTC Logo" width="50" height="50" />
+          <Pair>BTC/USD</Pair>
         </div>
-        <div className="col-1">
-          <Label>Change 24hr</Label>
-          <Value>{ticker.dailyChange} ({ticker.dailyChangePerc})</Value>
-        </div>
-        <div className="col-1">
-          <Label>High 24hr</Label>
-          <Value>{ticker.high}</Value>
-        </div>
-        <div className="col-1">
-          <Label>Low 24hr</Label>
-          <Value>{ticker.low}</Value>
-        </div>
-        <div className="col-1">
-          <Label>Volume 24hr</Label>
-          <Value>{ticker.volume}</Value>
+        <div className="row align-center">
+          <div className="col-1 ticker">
+            <Label>Last Price</Label>
+            <Value>{lastPrice.toFixed(1)}</Value>
+          </div>
+          <div className="col-1 ticker">
+            <Label>Change 24hr</Label>
+            <Value
+              color={dailyChange >= 0 ? 'lime' : 'red'}
+            >
+              {dailyChange.toFixed(2)} ({dailyChangePerc.toFixed(2)}%)
+            </Value>
+          </div>
+          <div className="col-1 ticker">
+            <Label>High 24hr</Label>
+            <Value>{high.toFixed(1)}</Value>
+          </div>
+          <div className="col-1 ticker">
+            <Label>Low 24hr</Label>
+            <Value>{low.toFixed(1)}</Value>
+          </div>
+          <div className="col-1 ticker">
+            <Label>Volume 24hr</Label>
+            <Value>{volume.toFixed(2)}</Value>
+          </div>
         </div>
       </div>
     );
